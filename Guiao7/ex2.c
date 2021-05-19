62e8369349ccf7db716c4155e15df0aa46c0f0ec
sucodlaranja <joaoandre210@mai.com>
2021-05-19 09:37:42 +0100

    aula 19/05

--- Guiao7/ex2.c
+++ Guiao7/ex2.c
@@ -3,14 +3,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char const *argv[])
 {
     int i = 2;
+    int pid[argc-2];
+    int status;
     while(i < argc) {
-        if(fork() == 0) {
+        if((pid[i-2] = fork()) == 0) {
             execlp("grep", "grep", argv[1],argv[i],NULL);
+            _exit(0);
         }
+        
+        
+        wait(&status);
+        if(status == 0) {
+            printf("Process with pid %d found the word (%d file)\n",pid[i-2],i-1);
+            for(int j = 0; j < argc-2;j++) {
+                if(pid[j] != pid[i-2] && pid[j] >= 0) {
+                kill(pid[j],SIGKILL);
+                printf("%d killed\n",pid[j]);
+                }
+            }
+            
+        }
+        
+        i++;
     }
     return 0;
 }
